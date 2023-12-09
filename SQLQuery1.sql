14a035ae5a5ce4986950e3d27b1eb3b52c012ae4
Mai Trung Tiến <86876887+anionzo@users.noreply.github.com>
2023-12-09 13:59:48 +0700

    comit alll

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+use Anionzo
+go
+
+select * from Pokemon 
+
+select * from Pokemon 
+where id like '5f770f5f-cc80-4b30-ba8d-31bc634f512c'
+
+select * from Reviews 
+where PokemonID like '5f770f5f-cc80-4b30-ba8d-31bc634f512c'
+
+select * from Owners
+select * from Categorys
+
+
+
+select * from PokemonCategories
+select * from PokemonOwners
