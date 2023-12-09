2c97d464b4231c2d38451c7f8d0df397c8cbda44
Mai Trung Tiến <86876887+anionzo@users.noreply.github.com>
2023-12-09 16:11:10 +0700

    comit interface, and category Repository

---
+++ ANIONZO-DA_THUCTAP/ANIONZO-API/Repository/InterfaceRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ANIONZO_API.Entity;
+using ANIONZO_API.Models;
+using static ANIONZO_API.Constants.WebApiEndpoint;
+
+namespace ANIONZO_API.Repository.InterfaceRepository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<CategoryEntity> GetAll();
+        CategoryEntity Get(string id);
+        ICollection<PokemonEntity> GetPokemonByCategory(string categoryId);
+        bool GetTExists(string id);
+        bool Create( CategoryEntity category);
+        bool Update(CategoryEntity category);
+        bool Delete(CategoryEntity category);
+        bool Save();
+    }
+}
