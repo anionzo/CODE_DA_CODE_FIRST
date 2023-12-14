a1cbd221a9fe21bef2692e71ba29c38c06c32828
Mai Trung Tiến <86876887+anionzo@users.noreply.github.com>
2023-12-15 00:37:29 +0700

    Interface Repository

---
+++ ANIONZO-DA_THUCTAP/ANIONZO-API/Repository/InterfaceRepository/IOwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using ANIONZO_API.Entity;
+using static ANIONZO_API.Constants.WebApiEndpoint;
+
+namespace ANIONZO_API.Repository.InterfaceRepository
+{
+    public interface IOwnerRepository
+    {
+        ICollection<OwnerEntity> GetAll();
+        OwnerEntity Get(string Id);
+        ICollection<OwnerEntity> GetOwnerOfAPokemon(string pokeId);
+        ICollection<PokemonEntity> GetPokemonByOwner(string ownerId);
+        bool OwnerExists(string Id);
+        bool Create(OwnerEntity owner);
+        bool Update(OwnerEntity owner);
+        bool Delete(OwnerEntity owner);
+        bool Save();
+    }
+}
